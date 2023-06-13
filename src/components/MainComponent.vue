88881ccefcb588888970991be0a2f80d8adb6b6a
Lip-7 <zhighofficial@gmail.com>
2023-06-13 15:54:47 +0200

    working on implementing projects

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,10 +1,14 @@
 <template>
     <main>
-        <HeroComponent/>
+        <HeroComponent />
+        <AboutComponent />
+        <ProjectsComponent />
     </main>
 </template>
   
 <script>
+import ProjectsComponent from './MainComponents/ProjectsComponents.vue'
+import AboutComponent from './MainComponents/AboutComponent.vue';
 import HeroComponent from './MainComponents/HeroComponent.vue';
 export default {
     name: 'MainComponent',
@@ -17,8 +21,11 @@ export default {
 
     },
     components: {
-    HeroComponent,
-},
+        HeroComponent,
+        AboutComponent,
+        ProjectsComponent,
+
+    },
     mounted() {
 
     }
--- src/components/MainComponents/AboutComponent.vue
+++ src/components/MainComponents/AboutComponent.vue
@@ -1,7 +1,30 @@
 <template>
-    <footer>
-
-    </footer>
+    <section id="about">
+        <div class="container">
+            <h2 class="section-title">About Me</h2>
+            <div class="row about-wrapper">
+                <div class="about-wrapper__image rounded-xl overflow-hidden">
+                    <img class="img-fluid" src="https://www.lagazzettadelmezzogiorno.it/resizer/1200/720/true/1547111366831.jpg--checco_zalone___niente_sanremo__vado_in_kenya_per_il_prossimo_film_.jpg?1547111366000" alt="Profile Image" width="450" height="350" />
+                </div>
+                <div class="about-wrapper__info">
+                    <p class="about-wrapper__info-text">
+                        Sono un aspirante full stack web developer di 27 anni proveniente da Napoli. Ho una formazione
+                        accademica nel campo del marketing e del management, ma la mia vera passione è lo sviluppo web. 
+                    </p>
+                    <p class="about-wrapper__info-text">
+                        Sono
+                        stato introdotto alle tecnologie web nel corso Boolean, dove ho acquisito una solida conoscenza dei
+                        principali linguaggi di programmazione e dei framework utilizzati nello sviluppo web. Ho una
+                        conoscenza approfondita di HTML, CSS, PHP e JavaScript, oltre ad avere familiarità con framework come
+                        React, Vue e Laravel.
+                    </p>
+                    <span class="about-wrapper__cta">
+                        <a href="#" class="cta-btn cta-btn--resume">View Resume</a>
+                    </span>
+                </div>
+            </div>
+        </div>
+    </section>
 </template>
   
 <script>
