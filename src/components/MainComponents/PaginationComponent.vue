5ef5c4d2d6c3781cd2111fb6dbd9db98246e732a
Lip-7 <zhighofficial@gmail.com>
2023-06-14 15:39:21 +0200

    minor fixes

--- src/components/MainComponents/PaginationComponent.vue
+++ src/components/MainComponents/PaginationComponent.vue
@@ -32,7 +32,7 @@
                     </a>
                     <!-- Current: "z-10 bg-indigo-600 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600", Default: "text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0" -->
                     <a href="#" v-for="page in store.lastPage" class="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0" 
-                    :class="page == store.currentPage ? 'bg-indigo-600 text-white' : ''" @click.prevent="GoPage(page)">
+                    :class="page == store.currentPage ? 'bg-[#4DA1E2] text-white' : ''" @click.prevent="GoPage(page)">
                         {{ page }}
                     </a>
 
